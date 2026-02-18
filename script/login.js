0a84aff1d913ca153e3bbc775f7d9242a966ae42
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-02-18 19:19:57 +0600

    login page done with funtionality

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById('btn')
+.addEventListener('click',function(){
+    const mobileNumber=document.getElementById('mobile-number');
+    const number=mobileNumber.value;
+    // console.log(number)
+    const pinNumber=document.getElementById('pin-number');
+    const pin=pinNumber.value;
+    // console.log(pin)
+    if(number==='01825692898'&&pin==='1234'){
+        alert('success');
+        window.location.assign('/home.html')
+    }
+    else{
+        alert('failed, try again');
+        return;
+    }
+
+})
